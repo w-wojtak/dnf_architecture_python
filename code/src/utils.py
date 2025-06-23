0c36245ca9eb4b522096883646c418c1458aa97f
Weronika Wojtak <w.wojtak@gmail.com>
2025-06-23 15:51:06 +0100

    moved functions to utils

---
+++ code/src/utils.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def kernel_osc(x, a, b, alpha):
+    return a * (np.exp(-b * abs(x)) * (b * np.sin(abs(alpha * x)) + np.cos(alpha * x)))
+
+
+def get_inputs(x, t, dt, input_pars, input_flag):
+    if not input_flag:
+        return np.zeros((len(t), len(x)))
+
+    [input_shape, input_positions, input_onsets, input_durations] = input_pars
+    amplitude, sigma = input_shape
+    inputs = np.zeros((len(t), len(x)))
+
+    for pos, onset, dur in zip(input_positions, input_onsets, input_durations):
+        time_on = int(onset / dt)
+        time_off = int((onset + dur) / dt)
+        gaussian = amplitude * np.exp(-((x - pos) ** 2) / (2 * sigma ** 2))
+        inputs[time_on:time_off, :] += gaussian
+
+    return inputs
