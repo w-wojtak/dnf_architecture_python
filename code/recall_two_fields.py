ae0d5e3b28b5b64370bd26cf1fbdf821a6ea6854
Weronika Wojtak <w.wojtak@gmail.com>
2025-06-26 11:29:32 +0100

    initializing fields from files

--- code/recall_two_fields.py
+++ code/recall_two_fields.py
@@ -9,8 +9,8 @@ from src.utils import *
 from pathlib import Path
 
 # Load most recent files
-data_dir = Path(__file__).resolve().parents[1] / "data"
-file1, file2 = find_latest_field_files(data_dir)
+# data_dir = Path(__file__).resolve().parents[1] / "data"
+# file1, file2 = find_latest_field_files(data_dir)
 
 # u_field_1 = np.load(file1)
 # u_field_2 = np.load(file2)
@@ -36,6 +36,9 @@ file1, file2 = find_latest_field_files(data_dir)
 
 # plt.tight_layout()
 # plt.show()
+
+trial_number = 1
+
 
 # ====================================
 # --------- Parameters ---------------
@@ -73,3 +76,65 @@ w_hat_sim = np.fft.fft(kernel_sim)
 # plt.grid(True)
 # plt.tight_layout()
 # plt.show()
+
+folder = "data"
+
+file1, ts1 = find_latest_file_with_prefix(folder, "u_field_1_")
+file2, ts2 = find_latest_file_with_prefix(folder, "u_field_2_")
+file3, ts3 = find_latest_file_with_prefix(folder, "u_d_")
+
+# Optional: check if all timestamps match
+if not (ts1 == ts2 == ts3):
+    raise ValueError("Timestamps do not match across all files.")
+
+# Load data
+u_field_1 = np.load(file1)
+u_field_2 = np.load(file2)
+u_d = np.load(file3)
+
+
+# plt.figure(figsize=(10, 4))
+# plt.plot(x, u_field_1, label='u 1')
+# plt.plot(x, u_d, label='u d', linestyle='--')
+# plt.xlabel('x')
+# plt.ylabel(' value')
+# plt.legend()
+# plt.grid(True)
+# plt.tight_layout()
+# plt.show()
+
+
+# load fields from files to initialize
+
+try:
+    # Flatten and compute initial h from task duration
+    u_d = u_d.flatten()
+    h_d_initial = max(u_d)
+
+    if trial_number == 1:
+        # Use u_field_2 for u_act
+        u_act = u_field_1.flatten() - h_d_initial + 1.5
+        input_action_onset = u_field_1.flatten()
+        h_u_act = -h_d_initial * np.ones(np.shape(x)) + 1.5
+
+        # Use u_field_1 for u_sim
+        u_sim = u_field_2.flatten() - h_d_initial + 1.5
+        input_action_onset_2 = u_field_2.flatten()
+        h_u_sim = -h_d_initial * np.ones(np.shape(x)) + 1.5
+
+    else:
+        data_dir = Path(os.getcwd()) / 'dnf_architecture_extended/data'
+        print(f"Loading h_amem from {data_dir}")
+
+        latest_h_amem_file, _ = find_latest_file_with_prefix(
+            data_dir, "h_amem_")
+        latest_h_amem = np.load(latest_h_amem_file, allow_pickle=True)
+
+        u_act = u_field_1.flatten() - h_d_initial + 1.5 + latest_h_amem
+        input_action_onset = u_field_1.flatten() + latest_h_amem
+        h_u_act = -h_d_initial * np.ones(np.shape(x)) + 1.5
+
+except FileNotFoundError:
+    print("No previous sequence memory found, initializing with default values.")
+    u_act = np.zeros(np.shape(x))
+    h_u_act = -3.2 * np.ones(np.shape(x))
